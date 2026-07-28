d1e6b42b8291af03f51d8ab3c78dd1f14b0ea386
mofukerlinerr <mofukerlinerr@gmail.com>
2026-07-29 10:43:17 +1200

    setup for player animation and generator charge added
    non built in imports
    -pygame
    
    added new folder for player animation images
    added supporting code for player animation
    new import from tkinter being -font for on screen texts
    added text on screen for generator charge
    changed clock speed to 50 a second to help with float math
    added new tag for player being -player_moving to check if player is moving for animations

--- PNP-DTC/DTCGame.py
+++ PNP-DTC/DTCGame.py
@@ -1,3 +1,4 @@
+from tkinter import font
 import pygame
 import sys
 
@@ -6,9 +7,13 @@ import sys
 # ----------------------------
 pygame.init()
 
+font = pygame.font.Font(None, 36)
+
+gen = 100
+
 player_speed = 5
 
-playerX = 50
+playerX = 0
 playerY = 50
 
 time = 0
@@ -34,28 +39,50 @@ while running:
 
     if keys[pygame.K_w]:
         playerY -= player_speed
+        player_moving = True
 
     if keys[pygame.K_a]:
         playerX -= player_speed
+        player_moving = True
 
     if keys[pygame.K_s]:
         playerY += player_speed
+        player_moving = True
 
     if keys[pygame.K_d]:
         playerX += player_speed
-
-    pygame.display.set_caption("Pygame Text Example")
+        player_moving = True
 
     tick += 1
+    time = tick/50
 
-    time = tick/60
+    for i in range(5):
+        animation_frame = i
+
+    player_an = pygame.image.load(f"playerstill{animation_frame}.png").convert_alpha()
+
+    if player_moving == True:
+        player_an = pygame.image.load(f"playermoving{animation_frame}.png").convert_alpha()
+
+        
+
+    
+
+    if time >= 10:
+        gen -= 0.02
+
+    if gen <= 0:
+        gen = 0
     
     screen.fill((255, 255, 255))
 
-    pygame.draw.rect(screen, (255, 0, 0), (playerX, playerY, 50, 50))
+    screen.blit(player_an, (playerX, playerY, 50, 50))
+
+    text_surface = font.render(f"{gen:.0f}", True, (0, 0, 0))
+    screen.blit(text_surface, (400, 300))
 
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(50)
 
 # ----------------------------
 # Shutdown area
